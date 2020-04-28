e4fef3cc26d4ffd3233ad16b61dbaa7ab71f1445
Leah Copeland <59579748+leah-is-offline@users.noreply.github.com>
2020-04-28 15:27:07 -0700

    timer added to bottom left screen

---
+++ Assets/timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+    public float timeStart = 60;
+    public Text textBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textBox.text = timeStart.ToString();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeStart -= Time.deltaTime;
+        textBox.text = Mathf.Round(timeStart).ToString();
+
+    }
+}
