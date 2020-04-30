d64d1f71fe7e97a7160087675c22ab64190548cd
ced888 <46654901+ced888@users.noreply.github.com>
2020-04-30 15:53:53 -0600

    Cedrit name prompt

---
+++ Assets/MainMenu_Scripts/NamePrompt.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NamePrompt : MonoBehaviour
+{
+    public InputField textbox;
+    public Button btn_OK;
+    public GameObject prompt;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        btn_OK.onClick.AddListener(ClosePrompt);
+    }
+
+    private void ClosePrompt()
+    {
+        prompt.SetActive(false);
+    }
+
+    void Start()
+    {
+       
+        if (Goal.GameOver)
+            prompt.SetActive(true);
+        else
+            prompt.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
